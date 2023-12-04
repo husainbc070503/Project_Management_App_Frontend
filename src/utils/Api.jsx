27226f3ce70c854b4fa1a42db4c75389c376f6ea
Husain Champawala <husainchampawala@gmail.com>
2023-12-04 17:34:11 +0530

    first commit

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,1 +1,2 @@
-export const api = "https://project-management-app-4kjw.onrender.com";
+// export const api = "https://project-management-app-4kjw.onrender.com";
+export const api = "http://localhost:8879";
